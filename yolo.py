f82e23f8c3a465de6523ede2f1c53ea601448c86
Osada-Yutaro <osada090909@icloud.com>
2019-03-21 12:32:33 +0900

    implement to save model

--- yolo.py
+++ yolo.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 import glob
-import gc
 
 import tensorflow as tf
 import numpy as np
@@ -12,7 +11,6 @@ C = 20
 INDICES = {'diningtable': 0, 'chair': 1, 'horse': 2, 'person': 3, 'tvmonitor': 4, 'bird': 5, 'cow': 6, 'dog': 7, 'bottle': 8, 'pottedplant': 9, 'aeroplane': 10, 'car': 11, 'cat': 12, 'sheep': 13, 'bicycle': 14, 'sofa': 15, 'boat': 16, 'train': 17, 'motorbike': 18, 'bus': 19}
 LAMBDA_COORD = 5.0
 LAMBDA_NOOBJ = 0.5
-keep_prob = 0.5
 BATCH_SIZE = 64
 
 
@@ -51,11 +49,13 @@ def load_dataset(train_size=0.75):
 
     return x_data[0:int(D*train_size)], x_data[int(D*train_size):D], y_data[0:int(D*train_size)], y_data[int(D*train_size):D]
 
-def weight_variable(shape):
-    return tf.Variable(tf.truncated_normal(shape, stddev=0.03))
+def weight_variable(shape, name):
+    with tf.variable_scope('yolo', reuse=False):
+        w = tf.get_variable(name, initializer=tf.truncated_normal(shape, stddev=0.01))
+        return w
 
-def conv2d(x, shape, step=1):
-    return tf.nn.conv2d(x, weight_variable(shape), [1, step, step, 1], 'SAME')
+def conv2d(x, shape, step=1, name=None):
+    return tf.nn.conv2d(x, weight_variable(shape, name), [1, step, step, 1], 'SAME')
 
 def leaky_relu(x):
     return tf.nn.leaky_relu(x, alpha=0.1)
@@ -64,50 +64,50 @@ def max_pool(x, r, step):
     return tf.nn.max_pool(x, [1, r, r, 1], [1, step, step, 1], 'SAME')
 
 def first_block(x):
-    return max_pool(leaky_relu(conv2d(x, [7, 7, 3, 192], 2)), 2, 2)
+    return max_pool(leaky_relu(conv2d(x, [7, 7, 3, 192], 2, name='w_1')), 2, 2)
 
 def second_block(x):
-    return max_pool(leaky_relu(conv2d(x, [3, 3, 192, 128], 1)), 2, 2)
+    return max_pool(leaky_relu(conv2d(x, [3, 3, 192, 128], 1, name='w_2')), 2, 2)
 
 def third_block(x):
-    layer_1 = leaky_relu(conv2d(x, [1, 1, 128, 256]))
-    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 256, 256]))
-    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 256, 512]))
-    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 512, 256]))
+    layer_1 = leaky_relu(conv2d(x, [1, 1, 128, 256], name='w_3_1'))
+    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 256, 256], name='w_3_2'))
+    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 256, 512], name='w_3_3'))
+    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 512, 256], name='w_3_4'))
     return max_pool(layer_4, 2, 2)
 
 def fourth_block(x):
-    layer_1 = leaky_relu(conv2d(x, [1, 1, 256, 512]))
-    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 512, 256]))
-    for _ in range(2):
-        layer_1 = leaky_relu(conv2d(layer_2, [1, 1, 256, 512]))
-        layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 512, 256]))
-    layer_1 = leaky_relu(conv2d(layer_2, [1, 1, 256, 512]))
-    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 512, 512]))
-
-    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 512, 1024]))
-    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 1024, 512]))
-    return max_pool(layer_4, 2, 2)
+    layer_1 = leaky_relu(conv2d(x, [1, 1, 256, 512], name='w_4_1'))
+    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 512, 256], name='w_4_2'))
+    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 256, 512], name='w_4_3'))
+    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 512, 256], name='w_4_4'))
+    layer_5 = leaky_relu(conv2d(layer_4, [1, 1, 256, 512], name='w_4_5'))
+    layer_6 = leaky_relu(conv2d(layer_5, [3, 3, 512, 256], name='w_4_6'))
+    layer_7 = leaky_relu(conv2d(layer_6, [1, 1, 256, 512], name='w_4_7'))
+    layer_8 = leaky_relu(conv2d(layer_7, [3, 3, 512, 512], name='w_4_8'))
+    layer_9 = leaky_relu(conv2d(layer_8, [1, 1, 512, 1024], name='w_4_9'))
+    layer_10 = leaky_relu(conv2d(layer_9, [3, 3, 1024, 512], name='w_4_10'))
+    return max_pool(layer_10, 2, 2)
 
 def fifth_block(x):
-    layer_1 = leaky_relu(conv2d(x, [1, 1, 512, 1024]))
-    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 1024, 512]))
-    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 512, 1024]))
-    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 1024, 1024]))
-    layer_5 = leaky_relu(conv2d(layer_4, [3, 3, 1024, 1024]))
-    return leaky_relu(conv2d(layer_5, [3, 3, 1024, 1024], 2))
+    layer_1 = leaky_relu(conv2d(x, [1, 1, 512, 1024], name='w_5_1'))
+    layer_2 = leaky_relu(conv2d(layer_1, [3, 3, 1024, 512], name='w_5_2'))
+    layer_3 = leaky_relu(conv2d(layer_2, [1, 1, 512, 1024], name='w_5_3'))
+    layer_4 = leaky_relu(conv2d(layer_3, [3, 3, 1024, 1024], name='w_5_4'))
+    layer_5 = leaky_relu(conv2d(layer_4, [3, 3, 1024, 1024], name='w_5_5'))
+    return leaky_relu(conv2d(layer_5, [3, 3, 1024, 1024], 2, name='w_5_6'))
 
 def sixth_block(x):
-    layer_1 = leaky_relu(conv2d(x, [3, 3, 1024, 1024]))
-    return leaky_relu(conv2d(layer_1, [3, 3, 1024, 1024]))
+    layer_1 = leaky_relu(conv2d(x, [3, 3, 1024, 1024], name='w_6_1'))
+    return leaky_relu(conv2d(layer_1, [3, 3, 1024, 1024], name='w_6_2'))
 
-def seventh_block(x):
-    w = weight_variable([7*7*1024, 4096])
+def seventh_block(x, keep_prob):
+    w = weight_variable([7*7*1024, 4096], name='w_7')
     conn = leaky_relu(tf.matmul(tf.reshape(x, [-1, 7*7*1024]), w))
     return tf.nn.dropout(conn, keep_prob)
 
 def eighth_block(x):
-    w = weight_variable([4096, 7*7*30])
+    w = weight_variable([4096, 7*7*30], name='w_8')
     return tf.nn.relu(tf.reshape(tf.matmul(x, w), [-1, 7, 7, 30]))
 
 def loss_5(output_target, output_pred):
@@ -151,11 +151,12 @@ def main():
     x = tf.placeholder(tf.float32, [None, 448, 448, 3])
     y = tf.placeholder(tf.float32, [None, S, S, 5*B + C])
     D = tf.placeholder(tf.int32)
+    keep_prob = tf.placeholder(tf.float32)
 
     train_data_size = x_train.shape[0]
     test_data_size = x_test.shape[0]
 
-    y_pred = eighth_block(seventh_block(sixth_block(fifth_block(fourth_block(third_block(second_block(first_block(x))))))))
+    y_pred = eighth_block(seventh_block(sixth_block(fifth_block(fourth_block(third_block(second_block(first_block(x)))))), keep_prob))
 
     err = loss(y, y_pred, D)
     train = tf.train.GradientDescentOptimizer(1e-7).minimize(err)
@@ -167,22 +168,27 @@ def main():
 
     with tf.Session() as sess:
         sess.run(init)
-        for epoch in range(135):
+        for epoch in range(1):
             count_train = 0
-            err_train = 0
             while count_train < train_data_size:
                 nextcount = min(count_train + BATCH_SIZE, train_data_size)
-                sess.run(train, feed_dict={x: x_train[count_train:nextcount], y: y_train[count_train:nextcount], D: nextcount - count_train})
-                err_train += sess.run(err, feed_dict={x: x_train[count_train:nextcount], y: y_train[count_train:nextcount], D: nextcount - count_train})
+                sess.run(train, feed_dict={x: x_train[count_train:nextcount], y: y_train[count_train:nextcount], D: nextcount - count_train, keep_prob: .5})
                 count_train = nextcount
+            if epoch%10 == 0:
+                count_train = 0
+                err_train = 0
+                while count_train < train_data_size:
+                    nextcount = min(count_train + BATCH_SIZE, train_data_size)
+                    err_train += sess.run(err, feed_dict={x: x_train[count_train:nextcount], y: y_train[count_train:nextcount], D: nextcount - count_train, keep_prob: 1.})
+                    count_train = nextcount
 
-            count_test = 0
-            err_test = 0
-            while count_test < test_data_size:
-                nextcount = min(count_test + BATCH_SIZE, test_data_size)
-                err_test += sess.run(err, feed_dict={x: x_test[count_test:nextcount], y: y_test[count_test:nextcount], D: nextcount - count_test})
-                count_test = nextcount
-            print('epoch=', epoch, ', ', 'train error=', err_train, ', ', 'validation error=', err_test, sep='')
-        saver.save(sess, 'model.ckpt')
+                count_test = 0
+                err_test = 0
+                while count_test < test_data_size:
+                    nextcount = min(count_test + BATCH_SIZE, test_data_size)
+                    err_test += sess.run(err, feed_dict={x: x_test[count_test:nextcount], y: y_test[count_test:nextcount], D: nextcount - count_test, keep_prob: 1.})
+                    count_test = nextcount
+                print('epoch=', epoch, ', ', 'train error=', err_train, ', ', 'validation error=', err_test, sep='')
+        saver.save(sess, 'model/weights.ckpt')
 
 main()
