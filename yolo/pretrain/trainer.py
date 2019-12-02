0148b778d6edba1dbf27c14c6633126d4c712c34
OsadaYutaro <>
2019-12-02 16:21:06 +0900

    save and restore

--- yolo/pretrain/trainer.py
+++ yolo/pretrain/trainer.py
@@ -14,20 +14,22 @@ def fit(data_dir, checkpoint_dir, epoch_size=10, lr=1e-4, start_epoch=1):
     y = tf.compat.v1.placeholder(tf.float32, [None, 1000])
     learning_rate = tf.compat.v1.placeholder(tf.float32)
     y_pred = network.graph_def(x)
-    err_d = tf.reduce_sum(tf.square(y - y_pred))/BATCH_SIZE
+    err_d = tf.reduce_sum(tf.square(y - y_pred))
     minimize = tf.compat.v1.train.MomentumOptimizer(learning_rate=lr, momentum=0.9).minimize(err_d)
 
-    saver = tf.compat.v1.train.Saver()
+    saver = tf.compat.v1.train.Saver(max_to_keep=10)
     init = tf.compat.v1.global_variables_initializer()
     config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(allow_growth=True))
     data = ImageNetData(data_dir)
 
     with tf.compat.v1.Session(config=config) as sess:
-        train.trainer.restore(sess, saver, init,
-                              os.path.join(checkpoint_dir, str(start_epoch - 1)))
         random.seed()
         if start_epoch == 1:
             print('#epoch, training error, validation error, learning rate')
+            sess.run(init)
+        else:
+            saver.restore(sess, os.path.join(checkpoint_dir, str(-start_epoch + 1)))
+
         for epoch in range(start_epoch, start_epoch + epoch_size):
             data.shuffle()
 
@@ -44,7 +46,7 @@ def fit(data_dir, checkpoint_dir, epoch_size=10, lr=1e-4, start_epoch=1):
                 while count_train < data.TRAIN_DATA_SIZE:
                     nextcount = min(count_train + BATCH_SIZE, data.TRAIN_DATA_SIZE)
                     x_train, y_train = data.load_train(count_train, nextcount)
-                    err_train += sess.run(err_d*BATCH_SIZE/data.TRAIN_DATA_SIZE,
+                    err_train += sess.run(err_d,
                                           feed_dict={x: x_train, y: y_train})
                     count_train = nextcount
 
@@ -53,10 +55,9 @@ def fit(data_dir, checkpoint_dir, epoch_size=10, lr=1e-4, start_epoch=1):
                 while count_validation < data.VALIDATION_DATA_SIZE:
                     nextcount = min(count_validation + BATCH_SIZE, data.VALIDATION_DATA_SIZE)
                     x_validation, y_validation = data.load_validation(count_validation, nextcount)
-                    err_validation += sess.run(err_d*BATCH_SIZE/data.VALIDATION_DATA_SIZE,
+                    err_validation += sess.run(err_d,
                                                feed_dict={x: x_validation, y: y_validation})
                     count_validation = nextcount
 
-                print(epoch, err_train, err_validation, lr)
-                os.mkdir(os.path.join(checkpoint_dir, str(epoch)))
-                saver.save(sess, os.path.join(checkpoint_dir, str(epoch), 'weights.ckpt'))
+                print(epoch, err_train/data.TRAIN_DATA_SIZE, err_validation/data.VALIDATION_DATA_SIZE, lr)
+                saver.save(sess, checkpoint_dir, global_step=epoch)
